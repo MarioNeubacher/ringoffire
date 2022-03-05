5346f0f2cfe471e38a6e300be036efa4a44d8d91
Mario Neubacher <neubacher.mario00@gmail.com>
2022-03-05 15:53:46 +0100

    game music

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -6,13 +6,9 @@ import { Router } from '@angular/router';
   templateUrl: './start-screen.component.html',
   styleUrls: ['./start-screen.component.scss']
 })
-export class StartScreenComponent implements OnInit {
+export class StartScreenComponent {
 
   constructor(public router: Router) { } 
-
-  ngOnInit(): void {
-    new Audio('assets/audio/music.mp3').play();
-  }
 
   newGame() {
     this.router.navigateByUrl('/game'); //path game in app routing module
