95418f95bfeaae807d766f2655512f949f8d01ef
Mario Neubacher <neubacher.mario00@gmail.com>
2022-04-13 17:30:27 +0200

    firebase bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,8 @@ import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { provideStorage,getStorage } from '@angular/fire/storage';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 @NgModule({
   declarations: [
