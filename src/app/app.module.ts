601250e217b34739fb3a8cb337d5e5ea1bea7984
Mario Neubacher <neubacher.mario00@gmail.com>
2022-04-14 11:37:22 +0200

    firemodule cant execute ng serve --open

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,12 +16,13 @@ import { MatInputModule } from '@angular/material/input';
 import { GameInfoComponent } from './game-info/game-info.component';
 import { MatCardModule } from '@angular/material/card';
 import { environment } from '../environments/environment';
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { AngularFireModule } from '@angular/fire/compat';
+/* import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { provideStorage,getStorage } from '@angular/fire/storage';
 import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+import 'firebase/compat/firestore'; */
 
 @NgModule({
   declarations: [
@@ -33,7 +34,7 @@ import 'firebase/compat/firestore';
     GameInfoComponent
   ],
   imports: [
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    AngularFireModule.initializeApp(environment.firebase),
     MatFormFieldModule,
     BrowserModule,
     AppRoutingModule,
@@ -44,7 +45,7 @@ import 'firebase/compat/firestore';
     MatDialogModule, 
     MatInputModule, 
     FormsModule, 
-    MatCardModule, provideDatabase(() => getDatabase()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())
+    MatCardModule, /* provideDatabase(() => getDatabase()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage()) */
   ],
   providers: [],
   bootstrap: [AppComponent]
